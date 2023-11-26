b5e0b164b2412a244bf6564f2e5997884e26390b
niladri27 <123537147+niladri27@users.noreply.github.com>
2023-11-26 06:10:52 +0000

    commit4

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -16,6 +16,6 @@ console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// ===  //strict check, values as well as datatypes are checked
 
-console.log("2" === 2);
+console.log("2" === 2);     // value as well as datatype is checked
